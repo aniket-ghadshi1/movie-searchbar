6bafb9c68fd8708579f61cec76676e0701677c8b
aniket <aniketghadshi1@gmail.com>
2023-07-25 21:58:54 +0530

    movie search bar

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='App'>
+      <div className='elements'>
+        <div>hello world</div>
+        <div>hey</div>
+      </div>
+   </div>
+  )
+}
+
+export default App
